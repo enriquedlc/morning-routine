a1b2f093c90de743117a48896e1da229e7fad607
enriquedlc <enriquedlc2d@gmail.com>
2025-05-19 17:02:14 +0100

    :test_tube::recycle: test name

--- index.test.ts
+++ index.test.ts
@@ -3,20 +3,8 @@ import { describe, it, vi, expect } from "vitest";
 import { MyMorningRoutine } from "./index.ts";
 
 describe("Morning routine", () => {
-  it("should print 'Do exercise' at 06:00", () => {
+  it("should print 'Do exercise' between 06:00 and 06:59", () => {
     const date = new Date("2023-10-01T06:00:00");
-
-    vi.useFakeTimers();
-    vi.setSystemTime(date);
-
-    const routine = new MyMorningRoutine();
-    const activity = routine.whatShouldIDoNow();
-
-    expect(activity).toBe("Do exercise");
-    vi.useRealTimers();
-  });
-  it("should print 'Do exercise' at 06:59", () => {
-    const date = new Date("2023-10-01T06:59:00");
 
     vi.useFakeTimers();
     vi.setSystemTime(date);
